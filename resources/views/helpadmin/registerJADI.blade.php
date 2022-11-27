cc7a20dddd791631b5afdf60eadcdab2e6c2f377
hendryar <e1900337@helplive.edu.my>
2022-11-27 18:31:59 +0800

    replaced clashing files

---
+++ resources/views/helpadmin/registerJADI.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SchoolHELP | School Registration Page</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="assets/AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="assets/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="assets/AdminLTE/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="register-logo">
+    <a href="assets/AdminLTE/index2.html">School<b>HELP</b></a>
+  </div>
+
+  <div class="card">
+    <div class="card-body register-card-body">
+      <p class="login-box-msg">Register a new School</p>
+
+      <form method="post">
+        @csrf
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="School Name" id=school_name name="school_name" value="{{ old('name') }}" required autocomplete="school_name" @error('school_name') is-invalid @enderror name="school_name" value="{{ old('name') }}" required autocomplete="school_name" autofocus>
+          @error('school_address')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+          <div class="input-group-append">
+            <div class="input-group-text">
+              {{-- <span class="fas fa-user"></span> --}}
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="School Address" id=school_address @error('school_address') is-invalid @enderror name="school_address" value="{{ old('school_address') }}" required autocomplete="school_address" autofocus>
+          @error('school_address')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+          <div class="input-group-append">
+            <div class="input-group-text">
+              {{-- <span class="fas fa-user"></span> --}}
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="School City" id=school_city  @error('school_city') is-invalid @enderror name="school_city" value="{{ old('school_city') }}" required autocomplete="school_city" autofocus>
+          @error('school_address')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+          <div class="input-group-append">
+            <div class="input-group-text">
+              {{-- <span class="fas fa-user"></span> --}}
+            </div>
+          </div>
+        </div>
+        
+        
+        <div class="container mx-10 text-center",>
+          <div class="col-md-20 text-center">
+            <button type="submit" class="btn btn-primary btn-block" value="Submit">Register School</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
+<!-- /.register-box -->
+
+<!-- jQuery -->
+<script src="assets/AdminLTE/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/AdminLTE/dist/js/adminlte.min.js"></script>
+</body>
+</html>
