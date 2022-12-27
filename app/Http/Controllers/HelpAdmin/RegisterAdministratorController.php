0f7b505828d160093591339d311df4fb5ebcb143
hendryar <e1900337@helplive.edu.my>
2022-12-27 22:05:46 +0800

    help admin registration fixed
    - help admin registration properly register admin's school
    - changed login controller so it displays the new welcome page aswell

--- app/Http/Controllers/HelpAdmin/RegisterAdministratorController.php
+++ app/Http/Controllers/HelpAdmin/RegisterAdministratorController.php
@@ -6,6 +6,7 @@ use App\Models\School;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class RegisterAdministratorController extends Controller
 {
@@ -18,7 +19,9 @@ class RegisterAdministratorController extends Controller
     }
 
     public function addAdmin(Request $request) {
-        $database = DB::table('users')->insert([
+        $school = School::find($request->school_id);
+        if(!$school) return redirect()->back()->with('error','School not found');
+        $user = User::create([
             'username' => $request->username,
             'password' => bcrypt($request->password),
             'name' => $request->fullname,
@@ -27,8 +30,9 @@ class RegisterAdministratorController extends Controller
             'staffID' => $request->staffID,
             'position' => $request->position,
             'role' => 'school_admin',
-            'school' => $request->school,
+            'school_id' => $request->school_id,
         ]);
+
         return redirect()->route('help_admin_home')->with('success','Administrator Has Been Successfully Registered');
     }
 }
